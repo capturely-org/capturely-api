c5c67d8edc3113a6e5d297714cae2d951fa1e280
emilesherrott <emilesherrott@gmail.com>
2026-05-07 15:22:32 +0100

    Adjust get / route

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
   res.status(200).json({
     "ok": true,
     "message": "Welcome to capturely API",
-    "creator": "Emile Edward Sherrott"
+    "creator": "Emile Sherrott"
   })
 })
 
